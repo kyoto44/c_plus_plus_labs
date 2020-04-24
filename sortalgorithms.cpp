0658956c92ad69d36ef8d8b15fe8b0be3496e7fa
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-04-24 20:54:20 +0500

    add sort algh's lab

---
+++ sortalgorithms.cpp
@@ -0,0 +1,170 @@
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+//Линейный поиск
+int Search(int *arr, int size, int requiredValue) {
+  for (int i = 0; i < size; ++i) {
+    if (arr[i] == requiredValue)
+      return arr[i];
+  }
+  return -1337;
+}
+
+void Swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+int Partition(int *arr, int begin, int end) {
+  int x = arr[end];
+  int i = begin - 1;
+
+  for (int j = begin; j <= end - 1; j++) {
+    if (arr[j] <= x) {
+      i++;
+      Swap(&arr[i], &arr[j]);
+    }
+  }
+  Swap(&arr[i + 1], &arr[end]);
+  return (i + 1);
+}
+
+//Нерекурсивная быстрая сортировка
+void QuickSort(int *array, int start, int end) {
+  int stack[end - start + 1];
+  int top = -1;
+
+  stack[++top] = start;
+  stack[++top] = end;
+
+  while (top >= 0) {
+    end = stack[top--];
+    start = stack[top--];
+
+    int p = Partition(array, start, end);
+
+    if (p - 1 > start) {
+      stack[++top] = start;
+      stack[++top] = p - 1;
+    }
+
+    if (p + 1 < end) {
+      stack[++top] = p + 1;
+      stack[++top] = end;
+    }
+  }
+}
+
+//Рекурсивная быстрая сортировка
+void QuickSortRecursive(int *arr, int begin, int end) {
+  if (begin < end) {
+    int q = Partition(arr, begin, end);
+    QuickSortRecursive(arr, begin, q - 1);
+    QuickSortRecursive(arr, q + 1, end);
+  }
+}
+
+//Бинарный поиск
+int BinarySearch(const int *arr, int value, int left, int right) {
+  while (right >= left) {
+    int mid = (right + left) / 2;
+    if (arr[mid] == value)
+      return mid;
+    if (arr[mid] > value)
+      right = mid;
+    if (arr[mid] < value)
+      left = mid + 1;
+  }
+  return -1;
+}
+
+//Рекурсивный бинарный поиск
+int RecursiveBinarySearch(int *arr, int value, int left, int right) {
+  int mid = (right + left) / 2;
+  if (arr[mid] == value)
+    return mid;
+  if (arr[mid] > value)
+    return RecursiveBinarySearch(arr, value, left, mid);
+  if (arr[mid] < value)
+    return RecursiveBinarySearch(arr, value, mid + 1, right);
+  return -1;
+}
+
+//Заполнение массива случайными числами
+void FillArrayRnd(int *arr, int size, int min, int max) {
+  std::default_random_engine generator(time(0));
+  std::uniform_int_distribution<int> random(min, max);
+  for (int i = 0; i < size; ++i) {
+    arr[i] = random(generator);
+  }
+}
+
+//Вывод нужного количества элементов
+void PrintElem(int *arr, int num) {
+  cout << "First " << num << " elements : ";
+  for (int i = 0; i < num; ++i) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int main() {
+  int testSearch[10000];
+  FillArrayRnd(testSearch, 10000, -1000, 1000);
+  cout << "Linear search result: " << Search(testSearch, 10000, 322) << endl;
+
+  int testQuickSort[100];
+  FillArrayRnd(testQuickSort, 100, -10, 10);
+  PrintElem(testQuickSort, 50);
+  QuickSort(testQuickSort, 0, 99);
+  PrintElem(testQuickSort, 50);
+
+  int testQuickSortRecursive[100];
+  FillArrayRnd(testQuickSortRecursive, 100, -10, 10);
+  PrintElem(testQuickSortRecursive, 50);
+  QuickSortRecursive(testQuickSortRecursive, 0, 99);
+  PrintElem(testQuickSortRecursive, 50);
+
+  int testBinarySearch[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  cout << "Binary search result: " << BinarySearch(testBinarySearch, 6, 0, 3)
+       << endl;
+  cout << "Recursive binary search result: "
+       << RecursiveBinarySearch(testBinarySearch, 1, 0, 9) << endl;
+
+  int timeComparsionTestArray[100];
+  FillArrayRnd(timeComparsionTestArray, 100, -10, 10);
+
+  auto startTimeLS = chrono::steady_clock::now();
+  Search(timeComparsionTestArray, 100, 6);
+  auto endTimeLS = chrono::steady_clock::now();
+
+  auto startTimeBS = chrono::steady_clock::now();
+  BinarySearch(timeComparsionTestArray, 6, 0, 100);
+  auto endTimeBS = chrono::steady_clock::now();
+
+  auto startTimeRBS = chrono::steady_clock::now();
+  RecursiveBinarySearch(timeComparsionTestArray, 6, 0, 100);
+  auto endTimeRBS = chrono::steady_clock::now();
+
+  auto benchmarkTimeLS =
+      chrono::duration_cast<chrono::nanoseconds>(endTimeLS - startTimeLS);
+  auto benchmarkTimeBS =
+      chrono::duration_cast<chrono::nanoseconds>(endTimeBS - startTimeBS);
+  auto benchmarkTimeRBS =
+      chrono::duration_cast<chrono::nanoseconds>(endTimeRBS - startTimeRBS);
+
+  cout << "Linear search benchmark time: " << benchmarkTimeLS.count() << " ns"
+       << endl;
+  cout << "Binary search benchmark time: " << benchmarkTimeBS.count() << " ns"
+       << endl;
+  cout << "Recursive binary search benchmark time: " << benchmarkTimeRBS.count()
+       << " ns" << endl;
+  return 0;
+}
